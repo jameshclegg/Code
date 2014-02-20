7800ae0fd90cf785812ee22286848fd2b8fa1e93
James Clegg <j.clegg11@imperial.ac.uk>
2014-02-20 16:39:50 +0000

    Migrated into Microscope repo

--- hologram_control/scheme2/setup_screen.m
+++
@@ -1,3 +0,0 @@
-Screen('Preference', 'VisualDebugLevel', 1);  %errors only
-Screen('Preference', 'SkipSyncTests', 2 );  %disable sync tests, not important for drawing
-Screen('Preference', 'SuppressAllWarnings', 1);
